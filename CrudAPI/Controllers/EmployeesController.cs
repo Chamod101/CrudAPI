951689db4231ee5028fb0179e8484a8d51f5f661
Chamod Dilushanka <chamoddperera99@gmail.com>
2023-03-27 09:10:26 +0530

    Add project files.

---
+++ CrudAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Employees()
+        {
+            var employees = new string[] { "emplyee1", "employee2", "employee3" };
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public IActionResult NewEmployee()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateEmployee()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteEmployee()
+        {
+            var somthingHappend = true;
+            if (somthingHappend) return BadRequest();
+
+            return Ok();
+        }
+    }
+}
